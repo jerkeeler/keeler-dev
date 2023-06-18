d9987cc29da7e7acf9110c1c556defffed155ec6
Jeremy Keeler <j@keeler.dev>
2023-06-18 10:33:46 -0400

    Add tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#24d1f8',
+          dark: '#1b96b4',
+        },
+      },
+    },
+  },
+  variants: {
+    borderStyle: ['responsive', 'hover'],
+    borderWidth: ['responsive', 'hover'],
+    margin: ['responsive', 'first', 'last'],
+    extend: {},
+  },
+  plugins: [],
+};
