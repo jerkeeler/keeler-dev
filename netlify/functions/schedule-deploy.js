8e5364da7c310cbc87554d40351ffd7394d6c413
Jeremy Keeler <j@keeler.dev>
2023-06-18 16:03:40 -0400

    comment out stuff

--- netlify/functions/schedule-deploy.js
+++ netlify/functions/schedule-deploy.js
@@ -1,4 +1,4 @@
-const { schedule } = require('@netlify/functions');
+// const { schedule } = require('@netlify/functions');
 const fetch = require('node-fetch');
 
 const BUILD_HOOK = process.env.BUILD_HOOK;
