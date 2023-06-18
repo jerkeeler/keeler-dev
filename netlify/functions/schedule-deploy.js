9f91e735ca7a64cb9c3f41a9b431508f3501caaf
Jeremy Keeler <j@keeler.dev>
2023-06-18 15:16:10 -0400

    Add netlify scheduled deploys

---
+++ netlify/functions/schedule-deploy.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+const BUILD_HOOK = process.env.BUILD_HOOK;
+
+const handler = async () => {
+  await fetch(BUILD_HOOK, {
+    method: 'POST',
+  }).then((response) => {
+    console.log('Build hook response:', response.json());
+  });
+
+  return {
+    statusCode: 200,
+  };
+};
+
+export { handler };
